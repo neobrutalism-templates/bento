636325b8e8153649376a2aaad4fc0cb48def46fe
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-05-01 17:46:33 +0200

    updated styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,23 +9,25 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        bg: '#daf5f0',
-        main: '#c4a1ff',
-        mainAccent: '#9e66ff',
+        bg: '#dfe5f2',
+        main: '#88aaee',
       },
       borderRadius: {
         base: '5px',
       },
       boxShadow: {
-        base: '4px 4px 0px 0px rgba(0,0,0,1)',
+        base: '4px -4px 0px 0px rgba(0,0,0,1)',
       },
       translate: {
         boxShadowX: '4px',
-        boxShadowY: '4px',
+        boxShadowY: '-4px',
       },
       fontWeight: {
-        base: '500',
-        heading: '700',
+        base: '400',
+        heading: '600',
+      },
+      screens: {
+        w450: { raw: '(max-width: 450px)' },
       },
     },
   },
