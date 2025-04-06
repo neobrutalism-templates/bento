51d1c163f5526fbbc4c176cc8d506f3b5a3517c7
Samuel Breznjak <samuelbreznjak35@gmail.com>
2025-04-06 19:55:37 +0200

    v4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <body className={workSans.className}>
         <ThemeProvider attribute="class" disableTransitionOnChange>
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,17 @@ import { ThemeSwitcher } from './theme-switcher'
 
 export default function Home() {
   return (
-    <div className="text-text dark:text-darkText relative mx-auto h-full w-[700px] max-w-full p-8 md:p-16 xl:w-[1400px]">
+    <div className="text-foreground relative mx-auto h-full w-[700px] max-w-full p-8 md:p-16 xl:w-[1400px]">
       <div className="mb-12 w-full xl:fixed xl:mb-0 xl:w-[500px]">
         <img
-          className="border-border dark:border-darkBorder h-28 w-28 rounded-full border-2 xl:h-[184px] xl:w-[184px]"
+          className="border-border h-28 w-28 rounded-full border-2 xl:h-[184px] xl:w-[184px]"
           src="/pfp.png"
           alt="profile picture"
         />
 
         <div className="mt-8">
-          <h2 className="text-3xl font-heading sm:text-[44px]">John Doe</h2>
-          <p className="mt-6 text-base font-base sm:text-xl">
+          <h2 className="font-heading text-3xl sm:text-[44px]">John Doe</h2>
+          <p className="font-base mt-6 text-base sm:text-xl">
             This is the bento neobrutalism template. Check the{' '}
             <a
               className="font-heading underline"
@@ -30,11 +30,11 @@ export default function Home() {
       <div className="justify-end xl:flex">
         <div
           id="grid-container"
-          className="text-text dark:text-text grid w-full grid-cols-2 gap-10 md:grid-cols-3 xl:w-1/2 xl:pb-16 w450:grid-cols-1 w450:gap-7"
+          className="text-foreground grid w-full grid-cols-1 gap-7 sm:grid-cols-2 sm:gap-10 md:grid-cols-3 xl:w-1/2 xl:pb-16"
         >
           {Object.keys(LINKS).map((key) => (
             <a
-              className="border-border dark:border-darkBorder shadow-light dark:shadow-dark rounded-base border-2 bg-main p-5 transition-all hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none dark:hover:shadow-none"
+              className="border-border shadow-shadow text-main-foreground rounded-base bg-main hover:translate-x-boxShadowX hover:translate-y-boxShadowY border-2 p-5 transition-all hover:shadow-none"
               key={key}
               target="_blank"
               href={LINKS[key].link}
@@ -44,10 +44,10 @@ export default function Home() {
                 src={LINKS[key].icon.src}
                 alt={LINKS[key].title}
               />
-              <p className="mt-3 text-lg font-heading sm:text-xl">
+              <p className="font-heading mt-3 text-lg sm:text-xl">
                 {LINKS[key].title}
               </p>
-              <p className="mt-1 text-sm font-base sm:text-base">
+              <p className="font-base mt-1 text-sm sm:text-base">
                 {LINKS[key].text}
               </p>
             </a>
